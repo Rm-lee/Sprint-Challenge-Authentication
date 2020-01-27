22ec5ecc4513c94d8425c93a5358228fcded1d3f
Roger Lee <rmlee0003@gmail.com>
2020-01-26 21:59:52 -0500

    tests

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -5,7 +5,10 @@
 const jwt = require('jsonwebtoken')
 const jsec = process.env.JWT_SECRET || 'bologne'
 module.exports = (req,res,next) =>{
+ if(!req.headers.token){
+  res.status(401).json({ you: 'shall not pass!' });
 
+ }
   const {token} = req.headers
   const decoded = token
   ? jwt.verify(token,jsec)
--- index.spec.js
+++ index.spec.js
@@ -1,11 +1,46 @@
 const supertest = require("supertest")
 const server = require("./index")
+describe("endpoints", () => {
+ //  test("register user", async () => {
+ //    const res = await supertest(server)
+ //      .post("/api/auth/register")
+ //      .send({ username: "nnsnff", password: "1234" });
+ //    expect(res.status).toBe(201);
+ //  });
 
-test("welecom", async () => {
- const res = await supertest(server).get("/api/auth/register")
- expect(res.status).toBe(201)
- expect(res.type).toBe("application/json")
- expect(res.body.message).toBe("Welcome")
- console.log(res)
+ //  test("register user returns username", async () => {
+ //   const res = await supertest(server)
+ //     .post("/api/auth/register")
+ //     .send({ username: "nefn", password: "1234" });
+ //     expect(res.body.username).toBe("nefn")
 
-})
+ // });
+
+ test("login user", async () => {
+  const res = await supertest(server)
+    .post("/api/auth/login")
+    .send({ username: "nefn", password: "1234" });
+    const token = res.body.token
+    expect(token)
+
+});
+test("login user response", async () => {
+ const res = await supertest(server)
+   .post("/api/auth/login")
+   .send({ username: "nefn", password: "1234" });
+   expect(res.type).toBe("application/json")
+
+});
+test("login user response", async () => {
+ const res = await supertest(server)
+   .get("/api/jokes")
+      expect(res.type).toBe("application/json")
+
+});
+test("login user response", async () => {
+ const res = await supertest(server)
+   .get("/api/jokes")
+      expect(res.body.you).toBe("shall not pass!")
+
+});
+}); 
